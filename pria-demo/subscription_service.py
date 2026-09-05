ef99050f263c207afaaa9bce78066e1f3500c77d
safdarhussaingenai <safdargenai@gmail.com>
2026-09-05 13:56:10 +0530

    Create subscription_service.py

---
+++ pria-demo/subscription_service.py
@@ -0,0 +1,8 @@
+def create_subscription(request):
+    """
+    Simple demo of Partner API subscription creation.
+    """
+    return {
+        "status_code": 201,
+        "message": "Subscription created successfully"
+    }
